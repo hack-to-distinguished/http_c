3eb0ec2f6f269d95af7e3465a86d0c9d7bb6c2d7
Chris33871 <christian.harries15@gmail.com>
2025-04-12 11:01:08 +0100

    start client connection + added error msg handling

--- chris.c
+++ chris.c
@@ -6,47 +6,65 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-/*create a listener function that when running just sends a basic response when pinged*/
-#define MYPORT "3490"  // the port users will be connecting to
-#define BACKLOG 10     // how many pending connections queue will hold
+#include <errno.h>
 
+#define MYPORT "3490"
+#define BACKLOG 10     // how many pending connections queue will hold
+#define CONN_TYPE "client"
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
 
 int main(int argc, char *argv[]) {
     struct addrinfo hints, *res;
     struct sockaddr_storage their_addr;
     socklen_t addr_size;
     int sockfd, new_sockfd;
+    int reuse_addr_flag = 1;
+    int *ptr_reuse_addr_flag = &reuse_addr_flag;
 
     memset(&hints, 0, sizeof hints); /* will just copy 0s*/
-    /*hints.ai_family = AF_UNSPEC;*/
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
     getaddrinfo(NULL, MYPORT, &hints, &res);
 
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, ptr_reuse_addr_flag, sizeof(reuse_addr_flag));
 
     /*int socket(int domain, int type, int protocol);  */
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
-    /*int bind(int sockfd, struct sockaddr *my_addr, int addrlen);*/
-    bind(sockfd, res->ai_addr, res->ai_addrlen);
+    if (CONN_TYPE == "client") {
+        int conn = connect(sockfd, res->ai_addr, res->ai_addrlen);
+        printf("starting client connection: %d\n", conn);
+        if (conn == -1) {
+            error("Unable to start connection");
+        }
+    } else {
+        int bind_conn = bind(sockfd, res->ai_addr, res->ai_addrlen); /*int bind(int sockfd, struct sockaddr *my_addr, int addrlen);*/
+        printf("starting server: %d\n", bind_conn);
 
-    listen(sockfd, BACKLOG);
 
-    while (1) {
-    /*int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen); */
-        new_sockfd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+        listen(sockfd, BACKLOG);
 
-        char *msg = "Chris was here!";
-        int len, bytes_sent;
-        len = strlen(msg);
-        char buf[32];
-        int bytes_recv;
+        while (1) {
+        /*int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen); */
+            new_sockfd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
 
-        /*int send(int sockfd, const void *msg, int len, int flags); */
-        /*bytes_sent = send(sockfd, msg, len, 0);*/
-        /*bytes_recv = recv(sockfd, buf, len, 0);*/
-        close(new_sockfd);
+            char *msg = "Chris was here!";
+            int len, bytes_sent;
+            len = strlen(msg);
+            char buf[32];
+            int bytes_recv;
+
+            /*int send(int sockfd, const void *msg, int len, int flags); */
+            send(sockfd, msg, len, 0);
+            /*bytes_recv = recv(sockfd, buf, len, 0);*/
+            close(new_sockfd);
+        }
     }
 
 }
