176eebc6bd77899219128c1d3121b3a5ac7ee65b
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2025-05-02 16:33:17 +0100

    setting up foundations for parsing http requests from client web browser...

--- threadedserver.c
+++ threadedserver.c
@@ -13,9 +13,14 @@
 #include <time.h>
 #include <unistd.h>
 
-#define MYPORT "8080"
+#define MYPORT "80"
 #define BACKLOG 50 // how many pending connections queue will hold
 #define BUFFER_SIZE 1024
+
+void error(const char *msg) {
+    perror(msg);
+    exit(0);
+}
 
 // custom struct to pass values
 typedef struct {
@@ -28,7 +33,9 @@ void handle_hello(int new_connection_fd) {
     // this just sends a simple html response packet when user enters the
     // correct url (formatting a HTTP response)
     char *ptr_http_hello_response =
-        "HTTP/1.1 200 OK\r\n"
+        "HTTP/1.1 200 OK\r\n" // status line, 2xx - success, 1xx -
+                              // informational, 3xx redirection, 4xx client
+                              // error, 5xx server error
         "Content-Type: text/html; charset=UTF-8\r\n\r\n"
         "<!DOCTYPE html>\r\n"
         "<html>\r\n"
@@ -42,6 +49,23 @@ void handle_hello(int new_connection_fd) {
     send(new_connection_fd, ptr_http_hello_response,
          strlen(ptr_http_hello_response), 0);
 }
+
+void receive_HTTP(int new_connection_fd) {
+    int bytes_recv;
+    char http_request_buffer[BUFFER_SIZE];
+    bytes_recv = recv(new_connection_fd, http_request_buffer,
+                      sizeof(http_request_buffer), 0);
+    if (bytes_recv <= 0) {
+        error("Error receiving message from client!");
+    } else {
+        printf("HTTP REQUEST PACKET (msg from client) RECEIVED: %s\n",
+               http_request_buffer);
+    }
+}
+
+void create_HTTP_response_packet(int new_connection_fd) {}
+
+void handle_HTTP_requests(int new_connection_fd) {}
 
 // sending simple byte messages: out of the scope right now as I will be
 // focussing on http stuff
@@ -75,11 +99,6 @@ void send_simple_byte_messages(int new_connection_fd) {
     free(ptr_peer_addr_in);
 }
 
-void error(const char *msg) {
-    perror(msg);
-    exit(0);
-}
-
 // void* generic pointer, allow return of any type of data
 // void *args generic input parameters
 void *server_thread_to_run(void *args) {
@@ -95,20 +114,12 @@ void *server_thread_to_run(void *args) {
     // here i made it artificially do work by just adding a random time delay so
     // it is actually easier to see the concurrency work in action with the
     // threads
-    // int delay_seconds = 1 + rand() % 6; // 1-3 seconds
-    // sleep(delay_seconds);
+    int delay_seconds = 1 + rand() % 6; // 1-3 seconds
+    sleep(delay_seconds);
 
     // receive the http packet from web browser and 'process' it (in this case
     // im just printing it out)
-    int bytes_recv;
-    char http_request_buffer[BUFFER_SIZE];
-    bytes_recv = recv(new_connection_fd, http_request_buffer,
-                      sizeof(http_request_buffer), 0);
-    if (bytes_recv < 0) {
-        error("Error receiving message from client!");
-    } else {
-        printf("HTTP REQUEST PACKET RECEIVED: %s\n", http_request_buffer);
-    }
+    receive_HTTP(new_connection_fd);
 
     // send http response!
     handle_hello(new_connection_fd);
