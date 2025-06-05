b4180abab1a625db9583074ae02d2bd2c0514944
Chris33871 <christian.harries15@gmail.com>
2025-06-05 07:52:45 +0100

    Instant message reception :fire:

--- persistent_client.c
+++ persistent_client.c
@@ -10,7 +10,7 @@
 #include <time.h>
 #include <unistd.h>
 #define MYPORT "8080"
-#define MAX_IN 20
+#define MAX_IN 2
 #define BUFFER_SIZE 1024
 
 
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
 
     const char *server = argv[1];
     struct addrinfo hints, *res;
-    int client_fd;
+    int server_fd;
     int reuse_addr_flag = 1;
 
     printf("Connecting to server: %s\n", server);
@@ -40,43 +40,37 @@ int main(int argc, char *argv[]) {
         error("getaddrinfo failed");
     }
 
-    client_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if (client_fd == -1) {
+    server_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if (server_fd == -1) {
         error("socket failed");
     }
 
-    setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR, &reuse_addr_flag,
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse_addr_flag,
                sizeof(reuse_addr_flag));
 
-    if (connect(client_fd, res->ai_addr, res->ai_addrlen) == -1) {
-        error("connection failed");
+    if (connect(server_fd, res->ai_addr, res->ai_addrlen) == -1) {
+        error("connection failed\n");
     } else {
-        printf("Waiting on the server\n\n");
+        printf("Connected to the server\n\n");
     }
 
 
     struct pollfd pfds[MAX_IN];
-    pfds[0].fd = client_fd;
-    pfds[0].events = POLLIN | POLLOUT;
+    pfds[0].fd = server_fd;
+    pfds[0].events = POLLIN;
+
+    pfds[1].fd = STDIN_FILENO;
+    pfds[1].events = POLLIN;
 
     char send_buf[BUFFER_SIZE], recv_buf[BUFFER_SIZE], init_buf[32];
-    int bytes_sent, bytes_recv, fd_count = 1;
+    int bytes_sent, bytes_recv, fd_count = 2;
 
-    bytes_recv = recv(client_fd, init_buf, 32, 0);
+    bytes_recv = recv(server_fd, init_buf, 32, 0);
     if (bytes_recv != -1) {
         printf("Messages from the server: %s\n", init_buf);
     }
 
-    // TODO: Next steps:
-    // - 2nd message onwards, users receive their own messages instead of those 
-    // from the other client
-    // - Add a pollin mechanism that checks the server for any new messages at 
-    // a regular interval so that users don't only receive messages after they've
-    // sent one.
-    // - Allow users to disconnect after they've sent a message and still 
-    // reconnect
     while (1) {
-
         int poll_count = poll(pfds, fd_count, -1);
         if (poll_count == -1) {
             error("Poll error");
@@ -88,7 +82,7 @@ int main(int argc, char *argv[]) {
                 bytes_recv = recv(pfds[0].fd, recv_buf, BUFFER_SIZE, 0);
                 printf("MESSAGE RECEIVED: %s\n", recv_buf);
             }
-            if (pfds[0].revents & POLLOUT) {
+            if (pfds[1].revents & POLLIN) {
                 printf("\nPress enter to send your message:\n");
                 fgets(send_buf, BUFFER_SIZE, stdin);
 
@@ -98,7 +92,5 @@ int main(int argc, char *argv[]) {
                 }
             }
         }
-
-   }
-
+    }
 }
