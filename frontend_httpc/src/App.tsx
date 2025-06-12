2aafb21795a121c9a4201ca10393b013c237151b
Chris33871 <christian.harries15@gmail.com>
2025-06-12 21:01:02 +0100

    changed from vanilla js to react with typescript

---
+++ frontend_httpc/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Web Chat View</h1>
+      <div>
+      <!--This dive should contain the chat feed of floating messages-->
+      <!--Should also contain the enter message chat box-->
+      </div>
+    </>
+  )
+}
+
+export default App
