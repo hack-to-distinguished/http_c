c05cfcb76e4894e6cd3216dfc32f8d81aac6a440
Chris33871 <christian.harries15@gmail.com>
2025-06-16 20:47:31 +0100

    :lipstick: basic ahh ugly ui

--- frontend_httpc/src/App.tsx
+++ frontend_httpc/src/App.tsx
@@ -3,11 +3,10 @@ import './App.css'
 import MessageBox from './messageBox.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Web Chat View</h1>
+      <h1 class="head_title">Web Chat View</h1>
       {/* The div below should contain the chat feed of floating messages */}
       {/*Should also contain the enter message chat box */}
       <div>
