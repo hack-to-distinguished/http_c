07b8d7af454e55e8323a69fe8d5176fc7f4616e0
Chris33871 <christian.harries15@gmail.com>
2025-06-16 08:11:03 +0100

    styling for message input

--- frontend_httpc/src/App.tsx
+++ frontend_httpc/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import MessageBox from './messageBox.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,9 +8,10 @@ function App() {
   return (
     <>
       <h1>Web Chat View</h1>
+      {/* The div below should contain the chat feed of floating messages */}
+      {/*Should also contain the enter message chat box */}
       <div>
-      <!--This dive should contain the chat feed of floating messages-->
-      <!--Should also contain the enter message chat box-->
+        <MessageBox/>
       </div>
     </>
   )
