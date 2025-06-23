6120c5ea028363b18cbafa1b46105358579c608a
Alejandro De Guzman <alejandrodeguzman@proton.me>
2025-06-23 18:13:46 +0100

    setting up header files

---
+++ threaded_server_src/http.h
@@ -0,0 +1,29 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+void HEADER_NAME_STATE(char **ptr_ptr_http_client_buffer, int new_connection_fd,
+                       bool host_header_present, char *ptr_uri,
+                       char *ptr_method);
+
+void HEADER_VALUE_STATE(char **ptr_ptr_http_client_buffer,
+                        int new_connection_fd, bool host_header_present,
+                        char *ptr_uri, char *ptr_method);
+
+void ERROR_STATE(int new_connection_fd);
+
+void REQUEST_LINE_STATE(char **ptr_ptr_http_client_buffer,
+                        int new_connection_fd);
+
+void STATE_PARSER(char *ptr_http_client_buffer, int new_connection_fd);
+
+void parse_HTTP_requests(int new_connection_fd);
+
+char *receive_HTTP_request(int new_connection_fd);
+
+void send_http_response(int new_connection_fd, char *ptr_packet_buffer);
+
+size_t get_size_of_file(FILE *fp);
+
+char *get_file_type_from_uri(char *ptr_uri_buffer);
+
+void send_requested_file_back(int new_connection_fd, char *ptr_uri_buffer);
---
+++ threaded_server_src/threadpool.h
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#define QUEUE_SIZE 2048
+
+typedef struct {
+    int sock_fd;
+} thread_config_t;
+
+typedef struct {
+    thread_config_t queue[QUEUE_SIZE];
+    size_t front_pointer;
+    size_t rear_pointer;
+    size_t queue_size;
+    pthread_mutex_t thread_pool_mutex_t;
+    pthread_cond_t thread_pool_cond_t;
+} thread_pool_t;
+
+void *server_thread_to_run(void *args);
+
+void thread_pool_enqueue_t(thread_config_t tct);
+
+void *worker_thread_t(void *args);
+
+void thread_pool_t_init();
