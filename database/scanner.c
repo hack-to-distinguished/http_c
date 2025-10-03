668cb4d89fd7535308febaa57900fa5ac6ad58bb
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2025-10-03 22:36:29 +0100

    .

--- database/scanner.c
+++ database/scanner.c
@@ -132,6 +132,8 @@ char *scanToken(char *currentPosOfLexeme, tokenListCTX *ctx,
                          getIdentifierLiteral(currentPosOfLexeme, bufferStart));
             }
             currentPosOfLexeme -= 1;
+        } else if (isAlpha(c)) {
+            printf("Alphabetical char detected!");
         } else {
             fprintf(stderr, "\nUnrecognised Input");
             exit(1);
