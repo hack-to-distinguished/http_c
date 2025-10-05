26e6c672d4ba0a41ba59825c0f0600359a204a5b
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2025-10-05 15:31:20 +0100

    supported lexical analysis of order by

--- database/tokenizer/token_list.c
+++ database/tokenizer/token_list.c
@@ -62,7 +62,7 @@ void destroyTokenList(tokenListCTX *ctx) {
     printf("\n");
     for (size_t i = 0; i < ctx->currentSize; i++) {
         printf("\nDestroying Token at address %p!", ctx->tail[i].self);
-        if (ctx->tail[i].type <= 13) {
+        if (ctx->tail[i].type <= 15) {
             free(ctx->tail[i].lexeme);
         }
         free(ctx->tail[i].self);
