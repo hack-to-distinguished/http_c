49c0e6ff5c7682e0771f11717c0e9c30aee2c7ca
Chris33871 <christian.harries15@gmail.com>
2025-10-15 08:16:14 +0100

    re-organising code + adding outline

--- database/storage/message_store.c
+++ database/storage/message_store.c
@@ -4,12 +4,21 @@
 #include <time.h>
 #include "message_store.h"
 
-void ds_view_all_entries(flat_message_store fms[MSG_STORE_SIZE])
+void ms_view_all_entries(flat_message_store* fms)
 {
 
-    int msg_num = MSG_STORE_SIZE;
-    printf("Number of messages saved: %d\n", msg_num);
-    for (int i = 0; i < msg_num; i++)
+    printf("Message Store sender_id: %s\n", fms->sender_id);
+    printf("Message Store recipient_id: %s\n", fms->recipient_id);
+    printf("Message Store msg_len: %zu\n", fms->msg_len);
+    printf("Message Store send_time: %jd\n", fms->send_time);
+    printf("Message Store recv_time: %jd\n", fms->recv_time);
+    printf("Message Store msg_type: %c\n", fms->msg_type);
+    // TODO: Fix the char pointer print above
+    printf("Message Store send_status: %zu\n", fms->send_status);
+    printf("Message Store recv_status: %zu\n", fms->recv_status);
+
+    int i = 1;
+    while (fms[i].item_count > fms[i - 1].item_count)
     {
         printf("Message Store sender_id: %s\n", fms[i].sender_id);
         printf("Message Store recipient_id: %s\n", fms[i].recipient_id);
@@ -20,24 +29,47 @@ void ds_view_all_entries(flat_message_store fms[MSG_STORE_SIZE])
         // TODO: Fix the char pointer print above
         printf("Message Store send_status: %zu\n", fms[i].send_status);
         printf("Message Store recv_status: %zu\n", fms[i].recv_status);
-        if (fms[i].last_item == 1)
-        {
-            break;
-        }
     }
 }
 
-// TODO: Implement resizing mechanism when the store fills up
-void ds_resize_store() { return; }
+// TODO: Create get latest entry
+void ms_stream_messages_desc()
+{
+    // TODO:
+    // - Read the saved data
+    // - Stream the results as they become available
+    return;
+}
+void ms_stream_user_messages_desc(char* sender_id)
+{
+    // TODO:
+    // - Read the saved data
+    // - Filter by sender_id (How does SQL have such fast filters)
+    // - Stream the results as they become available
+    return;
+}
 
-// TODO: implement the data stucture in the existing messaging system,
-// appending information to message store as they come in
-// - Add entry
-// - Get latest entry
-// - Get entry in reverse order
-void ds_add_message() { return; }
-void ds_stream_messages_desc() { return; }
-void ds_stream_user_messages_desc() { return; }
+// TODO: Implement a add message function
+void ms_add_message(char* sender_id, char* recipient_id, char* user_message,
+                    flat_message_store* fms)
+{
+    // TODO:
+    // - append the user message to the struct
+    // - move the item_count flag
+
+    fms->item_count = 0;
+    return;
+
+    // TODO: Should I create a linked list?
+    // Would that make appending faster?
+
+    // IMPROVEMENT:
+    // - Find out how to not rely on having a list end flag
+    // - This function needs to work in conflicting cases
+}
+
+// TODO: Implement resizing mechanism when the store fills up
+void ms_resize_store() { return; }
 
 // TODO: Add cleanup pointers
 
@@ -55,10 +87,10 @@ int main()
     fms[0].recv_time   = time(recieved_time);
     fms[0].send_status = 1;
     fms[0].recv_status = 1;
-    fms[0].last_item   = 1;
+    fms[0].item_count  = 1;
     // TEST: Adding this as an examples message
 
-    ds_view_all_entries(fms);
+    ms_view_all_entries(fms);
 
     return (0);
 }
--- database/storage/message_store.h
+++ database/storage/message_store.h
@@ -13,14 +13,12 @@ typedef struct
     char   msg_type;
     size_t send_status; // 0=Failed 1=Sent 2=Pending
     size_t recv_status; // 1=Received 2=Read
-    int    last_item;   // 1=Last item
+    int    item_count;  // Increment with each new item
 } flat_message_store;
-// INFO: When appending to the list,
-// mark the current item with 0
-// and the new item with 1
 
-void ds_view_all_entries(flat_message_store fms[MSG_STORE_SIZE]);
-void ds_resize_store();
-void ds_add_message();
-void ds_stream_messages_desc();
-void ds_stream_user_messages_desc();
+void ms_view_all_entries(flat_message_store* fms);
+void ms_resize_store();
+void ms_add_message(char* sender_id, char* recipient_id, char* user_message,
+                    flat_message_store* fms);
+void ms_stream_messages_desc();
+void ms_stream_user_messages_desc(char* sender_id);
